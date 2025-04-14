609ac8c17f22fee9ed63cd5ac843e7ccc1c70db9
Safat11 <safat3622@gmail.com>
2025-04-14 11:56:54 +0600

    Left Side nav with data from server and module summary

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -7,11 +7,11 @@ import bg from '../../../assets/bg.png'
 const RightNav = () => {
     return (
         <div>
-            <h4>Login With</h4>
+            <h4 className='mt-4'>Login With</h4>
             <Button className='mb-2' variant="outline-primary"><FaGoogle />Login with Google</Button>
             <Button variant="outline-secondary"><FaGithub />Login with Github</Button>
             <div>
-                <h4>Find Us On</h4>
+                <h4 className='mt-4'>Find Us On</h4>
                 <ListGroup>
                     <ListGroup.Item><FaFacebook />Facebook</ListGroup.Item>
                     <ListGroup.Item><FaInstagram />Instagram</ListGroup.Item>
