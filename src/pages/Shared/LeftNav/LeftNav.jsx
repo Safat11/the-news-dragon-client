3b1255855743396635a84ac6d784279a6b06e579
Safat11 <safat3622@gmail.com>
2025-06-15 14:07:15 +0600

    Client side code with API Url Update and React

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -5,7 +5,7 @@ const LeftNav = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() =>{
-        fetch('http://localhost:5000/categories')
+        fetch('https://the-news-dragon-server-amran-hossains-projects-83f8c215.vercel.app/categories')
         .then(res => res.json())
         .then(data => setCategories(data))
         .catch(error => console.error(error))
